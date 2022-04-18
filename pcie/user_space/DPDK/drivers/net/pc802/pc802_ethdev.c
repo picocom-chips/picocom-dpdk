f2919555b1c10bb945b2d7d7ce70d41cab672713
weid <wei.dai@picocom.com>
2022-04-18 18:02:30 +0800

    BSP-334: not start mailbox thread if no BAR 1
    So this commit also can work with verison 0 FSBL in
    which no BAR 1 maps PFI mailboxes.
    
    Signed-off-by: weid <wei.dai@picocom.com>

--- pcie/user_space/DPDK/drivers/net/pc802/pc802_ethdev.c
+++ pcie/user_space/DPDK/drivers/net/pc802/pc802_ethdev.c
@@ -1518,13 +1518,15 @@ eth_pc802_dev_init(struct rte_eth_dev *eth_dev)
     adapter->bar0_addr = (uint8_t *)pci_dev->mem_resource[0].addr;
     gbar = bar = (PC802_BAR_t *)adapter->bar0_addr;
 
-    adapter->mailbox_pfi   = (mailbox_exclusive *)((uint8_t *)pci_dev->mem_resource[1].addr + 0x580);
-    adapter->mailbox_ecpri = (mailbox_exclusive *)((uint8_t *)pci_dev->mem_resource[2].addr);
-    for (dsp = 0; dsp < 3; dsp++) {
-        adapter->mailbox_dsp[dsp] = (mailbox_exclusive *)((uint8_t *)pci_dev->mem_resource[0].addr + 0x2000 + 0x400 * dsp);
-    }
+    if (NULL != pci_dev->mem_resource[1].addr) {
+        adapter->mailbox_pfi   = (mailbox_exclusive *)((uint8_t *)pci_dev->mem_resource[1].addr + 0x580);
+        adapter->mailbox_ecpri = (mailbox_exclusive *)((uint8_t *)pci_dev->mem_resource[2].addr);
+        for (dsp = 0; dsp < 3; dsp++) {
+            adapter->mailbox_dsp[dsp] = (mailbox_exclusive *)((uint8_t *)pci_dev->mem_resource[0].addr + 0x2000 + 0x400 * dsp);
+        }
 
-    pthread_create(&tid, NULL, pc802_mailbox, adapter);
+        pthread_create(&tid, NULL, pc802_mailbox, adapter);
+    }
 
     int socket_id = eth_dev->device->numa_node;
     uint32_t tsize = PC802_DEBUG_BUF_SIZE;
