ba209c5703cf6dff30f8cec6c7c32776e48f4c2e
weid <wei.dai@picocom.com>
2022-08-26 14:44:34 +0800

    SI-939: format drivers/net/pc802/pc802_ethdev.h
    Signed-off-by: weid <wei.dai@picocom.com>

--- drivers/net/pc802/pc802_ethdev.h
+++ drivers/net/pc802/pc802_ethdev.h
@@ -61,26 +61,26 @@ typedef struct PC802_BAR_t {
         PC802_CacheLine_t _cahce_line_repcnt;
         uint32_t REPCNT[PC802_TRAFFIC_NUM];
     };
-	union {
+    union {
         PC802_CacheLine_t _cahce_line_brccnt;
-		struct {
-			uint32_t BOOTSRCL;
-			uint32_t BOOTSRCH;
-			uint32_t BOOTDST;
-			uint32_t BOOTSZ;
-			uint32_t BOOTRCCNT;
-			uint32_t BOOTRSPL;
-			uint32_t BOOTRSPH;
-		};
+        struct {
+            uint32_t BOOTSRCL;
+            uint32_t BOOTSRCH;
+            uint32_t BOOTDST;
+            uint32_t BOOTSZ;
+            uint32_t BOOTRCCNT;
+            uint32_t BOOTRSPL;
+            uint32_t BOOTRSPH;
+        };
     };
-	union {
+    union {
         PC802_CacheLine_t _cahce_line_bepcnt;
-		struct {
-			uint32_t BOOTEPCNT;
-			uint32_t BOOTERROR;
+        struct {
+            uint32_t BOOTEPCNT;
+            uint32_t BOOTERROR;
             uint32_t BOOTDEBUG;
             uint32_t MB_HANDSHAKE;
-		};
+        };
     };
     union {
         PC802_CacheLine_t _cahce_line_macaddr;
