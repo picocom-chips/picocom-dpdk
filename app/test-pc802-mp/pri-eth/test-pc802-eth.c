eb6a9e9f531d934400c90e43c9c90f151337dd84
weid <wei.dai@picocom.com>
2022-10-20 18:32:40 +0800

    SI-1211: remove unused elements in test-pc802-eth.c
    Signed-off-by: weid <wei.dai@picocom.com>

--- app/test-pc802-mp/pri-eth/test-pc802-eth.c
+++ app/test-pc802-mp/pri-eth/test-pc802-eth.c
@@ -97,17 +97,9 @@ static const struct rte_eth_conf dev_conf = {
 #ifdef MULTI_PC802
 #define PCXX_CALL0(fun,dev) fun(dev)
 #define PCXX_CALL(fun,dev,cell) fun(dev,cell)
-static uint32_t process_ul_ctrl_msg(const char* buf, uint32_t payloadSize, uint16_t dev_index, uint16_t cell_index );
-static uint32_t process_dl_ctrl_msg(const char* buf, uint32_t payloadSize, uint16_t dev_index, uint16_t cell_index );
-static uint32_t process_ul_data_msg(const char* buf, uint32_t payloadSize, uint16_t dev_index, uint16_t cell_index );
-static uint32_t process_dl_data_msg(const char* buf, uint32_t payloadSize, uint16_t dev_index, uint16_t cell_index );
 #else
 #define PCXX_CALL0(fun,dev) fun()
 #define PCXX_CALL(fun,dev,cell) fun()
-static uint32_t process_ul_ctrl_msg(const char* buf, uint32_t payloadSize);
-static uint32_t process_dl_ctrl_msg(const char* buf, uint32_t payloadSize);
-static uint32_t process_ul_data_msg(const char* buf, uint32_t payloadSize);
-static uint32_t process_dl_data_msg(const char* buf, uint32_t payloadSize);
 #endif
 
 uint16_t g_pc802_index = 0;
@@ -358,84 +350,7 @@ static void swap_msg(uint32_t *a, uint32_t msgSz)
 static PC802_Traffic_Type_e QID_DATA[CELL_NUM_PRE_DEV] = { PC802_TRAFFIC_DATA_1, PC802_TRAFFIC_DATA_2};
 static PC802_Traffic_Type_e QID_CTRL[CELL_NUM_PRE_DEV] = { PC802_TRAFFIC_CTRL_1, PC802_TRAFFIC_CTRL_2};
 
-static union {
-    const char *cc;
-    uint32_t   *up;
-} dl_a[PC802_INDEX_MAX][CELL_NUM_PRE_DEV][17];
-static uint32_t dl_a_num[PC802_INDEX_MAX][CELL_NUM_PRE_DEV] = {0};
-
 static int atl_test_result[PC802_INDEX_MAX][CELL_NUM_PRE_DEV] = {0};
-
-#ifdef MULTI_PC802
-static uint32_t process_dl_ctrl_msg(const char* buf, uint32_t payloadSize, uint16_t dev_index, uint16_t cell_index )
-{
-#else
-static uint32_t process_dl_ctrl_msg(const char* buf, uint32_t payloadSize)
-{
-    uint16_t dev_index = 0;
-    uint16_t cell_index = 0;
-#endif
-    payloadSize = payloadSize;
-    dl_a[dev_index][cell_index][dl_a_num[dev_index][cell_index]].cc = buf;
-    dl_a_num[dev_index][cell_index]++;
-    return 0;
-}
-
-#ifdef MULTI_PC802
-static uint32_t process_ul_ctrl_msg(const char* buf, uint32_t payloadSize, uint16_t dev_index, uint16_t cell_index )
-{
-#else
-static uint32_t process_ul_ctrl_msg(const char* buf, uint32_t payloadSize)
-{
-    uint16_t dev_index = 0;
-    uint16_t cell_index = 0;
-#endif
-    uint64_t addr = (uint64_t)buf;
-    uint32_t *ul_msg = (uint32_t *)addr;
-    swap_msg(ul_msg, payloadSize);
-    uint32_t *dl_msg;
-    dl_msg = dl_a[dev_index][cell_index][dl_a_num[dev_index][cell_index] - 1].up;
-    if (check_same(&dl_msg, 1, ul_msg)) {
-        atl_test_result[dev_index][cell_index] |= 1;
-    }
-    dl_a_num[dev_index][cell_index] = 0;
-    return payloadSize;
-}
-
-#ifdef MULTI_PC802
-static uint32_t process_dl_data_msg(const char* buf, uint32_t payloadSize, uint16_t dev_index, uint16_t cell_index )
-{
-#else
-static uint32_t process_dl_data_msg(const char* buf, uint32_t payloadSize)
-{
-    uint16_t dev_index = 0;
-    uint16_t cell_index = 0;
-#endif
-    payloadSize = payloadSize;
-    dl_a[dev_index][cell_index][dl_a_num[dev_index][cell_index]].cc = buf;
-    dl_a_num[dev_index][cell_index]++;
-    return 0;
-}
-
-#ifdef MULTI_PC802
-static uint32_t process_ul_data_msg(const char* buf, uint32_t payloadSize, uint16_t dev_index, uint16_t cell_index )
-{
-#else
-static uint32_t process_ul_data_msg(const char* buf, uint32_t payloadSize)
-{
-    uint16_t dev_index = 0;
-    uint16_t cell_index = 0;
-#endif
-    uint64_t addr = (uint64_t)buf;
-    uint32_t *ul_msg = (uint32_t *)addr;
-    swap_msg(ul_msg, payloadSize);
-    uint32_t **dl_msg;
-    dl_msg = &dl_a[dev_index][cell_index][0].up;
-    if (check_same(dl_msg, dl_a_num[dev_index][cell_index] - 1, ul_msg)) {
-        atl_test_result[dev_index][cell_index] |= 2;
-    }
-    return payloadSize;
-}
 
 static int case1(void)
 {
