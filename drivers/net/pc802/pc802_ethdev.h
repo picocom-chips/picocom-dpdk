e0a54edb159f13e89a499d3f7a0a43b6fb33cad1
weid <wei.dai@picocom.com>
2023-04-25 16:03:17 +0800

    NDF-116: fix pc802 mailbox timestamp
    Signed-off-by: weid <wei.dai@picocom.com>

--- drivers/net/pc802/pc802_ethdev.h
+++ drivers/net/pc802/pc802_ethdev.h
@@ -412,14 +412,14 @@ typedef enum {
 
 typedef struct {
     uint32_t         action;                 // 0x00
+    uint32_t         num_args;               // 0x04
     union {
         uint64_t     time;
         struct {
-            uint32_t         num_args;               // 0x04
             uint32_t         retval;                 // 0x08
+            uint32_t         error;                  // 0x0C
         };
     };
-    uint32_t         error;                  // 0x0C
     uint32_t         arguments[MB_NUM_ARGS]; // 0x10+
 } magic_mailbox_t;
 
