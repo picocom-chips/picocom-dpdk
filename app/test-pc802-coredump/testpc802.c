268706aadb9990f5e8c389e780f32e39833df185
weid <wei.dai@picocom.com>
2024-03-19 11:54:16 +0800

    NDF-196: test npu trigger core dump
    Signed-off-by: weid <wei.dai@picocom.com>

---
+++ app/test-pc802-coredump/testpc802.c
@@ -0,0 +1,84 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2018-2020 Picocom Corporation
+ */
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <time.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdbool.h>
+
+#include <sys/queue.h>
+#include <sys/stat.h>
+
+#include <stdint.h>
+#include <unistd.h>
+#include <inttypes.h>
+
+#include <rte_common.h>
+#include <rte_errno.h>
+#include <rte_byteorder.h>
+#include <rte_log.h>
+#include <rte_debug.h>
+#include <rte_cycles.h>
+#include <rte_memory.h>
+#include <rte_memcpy.h>
+#include <rte_launch.h>
+#include <rte_eal.h>
+#include <rte_alarm.h>
+#include <rte_per_lcore.h>
+#include <rte_lcore.h>
+#include <rte_atomic.h>
+#include <rte_branch_prediction.h>
+#include <rte_mempool.h>
+#include <rte_malloc.h>
+#include <rte_mbuf.h>
+#include <rte_mbuf_pool_ops.h>
+#include <rte_interrupts.h>
+#include <rte_pci.h>
+#include <rte_ether.h>
+#include <rte_ethdev.h>
+#include <rte_dev.h>
+#include <rte_string_fns.h>
+
+#include <cmdline_rdline.h>
+#include <cmdline_parse.h>
+#include <cmdline_socket.h>
+#include <cmdline.h>
+
+#include <rte_pmd_pc802.h>
+#include <pcxx_ipc.h>
+#include <pcxx_oam.h>
+
+int main(int argc, char** argv)
+{
+    int diag;
+    int port_id = 0;
+    int pc802_index = 0;
+
+    printf("%s\n", picocom_pc802_version());
+    printf("PC802 Driver Tester built AT %s ON %s\n", __TIME__, __DATE__);
+
+    //signal(SIGINT, signal_handler);
+    //signal(SIGTERM, signal_handler);
+
+    diag = rte_eal_init(argc, argv);
+    if (diag < 0)
+        rte_panic("Cannot init EAL\n");
+
+    if (RTE_PROC_PRIMARY == rte_eal_process_type()) {
+        printf("Please run this program in secondary process !\n");
+        return -1;
+    }
+
+    pc802_trigger_coredump_from_npu(0, 31);
+
+    return 0;
+}
